f24d21afc9a86cffc2f1fff696555eed91b937a0
ywsfalmrb29-jpg <ywsfalmrb29@gmail.com>
2026-09-20 14:18:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// ===== الإعدادات =====
+const API_URL = window.location.origin;
+
+const BOT_TYPES = [
+  { id: 'store',     icon: '🛒', name: 'بوت متجر', desc: 'متجر إلكتروني كامل' },
+  { id: 'video',     icon: '🎬', name: 'بوت في
