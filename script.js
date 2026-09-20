b8a5ce442eb32c13432ebec82394e95e18da130d
ywsfalmrb29-jpg <ywsfalmrb29@gmail.com>
2026-09-20 14:30:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// ===== الإعدادات =====
-const API_URL = window.location.origin;
-
-const BOT_TYPES = [
-  { id: 'store',     icon: '🛒', name: 'بوت متجر', desc: 'متجر إلكتروني كامل' },
-  { id: 'video',     icon: '🎬', name: 'بوت في
